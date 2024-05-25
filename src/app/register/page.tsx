2e8bd9fbfd21dace63385dbf432957404f928516
nicchy123 <nasirchy252@gmail.com>
2024-05-25 23:55:00 +0600

    first commit

---
+++ src/app/register/page.tsx
@@ -0,0 +1,171 @@
+"use client";
+import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { userLogin } from "@/services/actions/userLogin";
+import { storeUserInfo } from "@/services/auth.services";
+import { toast } from "sonner";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import VCForm from "@/components/Forms/VCForm";
+import VCInput from "@/components/Forms/VCInput";
+
+export const validationSchema = z.object({
+  email: z.string().email("Please enter a valid email address!"),
+  password: z.string().min(6, "Must be at least 6 characters"),
+  confirmPassword: z.string(),
+  username: z.string(),
+});
+
+const RegisterPage = () => {
+  const [error, setError] = useState("");
+
+  const handleLogin = async (values: FieldValues) => {
+    try {
+      const res = await userLogin(values);
+      console.log(res);
+      if (res?.data?.token) {
+        toast.success(res?.message);
+        storeUserInfo({ accessToken: res?.data?.accessToken });
+        // router.push("/dashboard");
+      } else {
+        setError(res.message);
+      }
+    } catch (err: any) {
+      toast.error(err.message);
+    }
+  };
+
+  return (
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Register Voyage Companion
+              </Typography>
+            </Box>
+          </Stack>
+
+          {error && (
+            <Box>
+              <Typography
+                sx={{
+                  backgroundColor: "red",
+                  padding: "1px",
+                  borderRadius: "2px",
+                  color: "white",
+                  marginTop: "5px",
+                }}
+              >
+                {error}
+              </Typography>
+            </Box>
+          )}
+
+          <Box>
+            <VCForm
+              onSubmit={handleLogin}
+              resolver={zodResolver(validationSchema)}
+              defaultValues={{
+                email: "",
+                username: "",
+                password: "",
+                confirmPassword: "",
+              }}
+            >
+              <Grid container spacing={2} my={1}>
+                <Grid item xl={6} lg={6} sm={12} md={6} xs={12}>
+                  <VCInput
+                    name="username"
+                    label="User Name"
+                    type="text"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item xl={6} lg={6} sm={12} md={6} xs={12}>
+                  <VCInput
+                    name="email"
+                    label="Email"
+                    type="email"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item xl={6} lg={6} sm={12} md={6} xs={12}>
+                  <VCInput
+                    name="password"
+                    label="Password"
+                    type="password"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item xl={6} lg={6} sm={12} md={6} xs={12}>
+                  <VCInput
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    type="password"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+
+              <Link href={"/forgot-password"}>
+                <Typography
+                  mb={1}
+                  textAlign="end"
+                  component="p"
+                  fontWeight={300}
+                  sx={{
+                    textDecoration: "underline",
+                  }}
+                >
+                  Forgot Password?
+                </Typography>
+              </Link>
+
+              <Button
+                variant="contained"
+                sx={{
+                  margin: "10px 0px",
+                }}
+                fullWidth={true}
+                type="submit"
+              >
+                Register
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Don&apos;t have an account?{" "}
+                <Link href="/register">Create an account</Link>
+              </Typography>
+            </VCForm>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
+
+export default RegisterPage;
--- src/services/actions/userLogin.ts
+++ src/services/actions/userLogin.ts
@@ -1,30 +1,25 @@
 // "use server";
 
-import { FieldValues } from 'react-hook-form';
-import setAccessToken from './setAccessToken';
+import { FieldValues } from "react-hook-form";
+import setAccessToken from "./setAccessToken";
 
 export const userLogin = async (data: FieldValues) => {
-   const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/login`,
-      {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         body: JSON.stringify(data),
-         credentials: 'include',
-         // cache: "no-store",
-      }
-   );
-   const userInfo = await res.json();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    credentials: "include",
+    // cache: "no-store",
+  });
+  const userInfo = await res.json();
 
+  if (userInfo.data.token) {
+    setAccessToken(userInfo.data.token, {
+      redirect: "/dashboard",
+    });
+  }
 
-   console.log(userInfo);
-   if (userInfo.data.token) {
-      setAccessToken(userInfo.data.token, {
-         redirect: '/dashboard',
-      });
-   }
-
-   return userInfo;
+  return userInfo;
 };
